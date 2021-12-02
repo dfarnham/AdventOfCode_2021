f9dbf348a9b6231b5624775bd45faccfe890fb67
Dave Farnham <dave.farnham@icloud.com>
2021-12-01 22:23:20 -0700

    use into_iter() on range, cleanup tests

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,9 +1,11 @@
 use general::read_int_lines;
 use structopt::StructOpt;
 
+// https://adventofcode.com/2021/day/1
+
 // Given an input array:
 // Count the number of times the sum of measurements in a provided sliding window increases
-fn increase_window_count(array: &[i32], window: usize) -> usize {
+fn count_window_increase(array: &[i32], window: usize) -> usize {
     assert!(window > 0, "Window must be > 0");
     assert!(
         array.len() > window,
@@ -11,8 +13,10 @@ fn increase_window_count(array: &[i32], window: usize) -> usize {
         array.len(),
         window
     );
+
     (0..(array.len() - window))
-        .filter(|i| array[*i..(*i + window)].iter().sum::<i32>() < array[(*i + 1)..=(*i + window)].iter().sum::<i32>())
+        .into_iter()
+        .filter(|&i| array[i..(i + window)].iter().sum::<i32>() < array[(i + 1)..=(i + window)].iter().sum::<i32>())
         .count()
 }
 
@@ -36,8 +40,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // ==============================================================
 
     let measurements = read_int_lines(args.input)?;
-    println!("Answer Part 1 = {}", increase_window_count(&measurements, 1));
-    println!("Answer Part 2 = {}", increase_window_count(&measurements, 3));
+    println!("Answer Part 1 = {}", count_window_increase(&measurements, 1));
+    println!("Answer Part 2 = {}", count_window_increase(&measurements, 3));
     Ok(())
 }
 
@@ -45,53 +49,60 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 #[should_panic]
 fn empty_array() {
     let measurements = vec![];
-    increase_window_count(&measurements, 1);
+    let window = 1;
+    count_window_increase(&measurements, window);
 }
 
 #[test]
 #[should_panic]
 fn array_too_small() {
     let measurements = vec![199];
-    increase_window_count(&measurements, 1);
+    let window = 1;
+    count_window_increase(&measurements, window);
 }
 
 #[test]
 #[should_panic]
 fn invalid_window() {
     let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-    increase_window_count(&measurements, 0);
+    let window = 0;
+    count_window_increase(&measurements, window);
 }
 
 #[test]
 fn part1_example() {
     let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-    assert_eq!(increase_window_count(&measurements, 1), 7);
+    let window = 1;
+    assert_eq!(count_window_increase(&measurements, window), 7);
 
     let file = Some(std::path::PathBuf::from("input-example"));
     let measurements = read_int_lines(file).unwrap();
-    assert_eq!(increase_window_count(&measurements, 1), 7);
+    assert_eq!(count_window_increase(&measurements, window), 7);
 }
 
 #[test]
 fn part2_example() {
     let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-    assert_eq!(increase_window_count(&measurements, 3), 5);
+    let window = 3;
+    assert_eq!(count_window_increase(&measurements, window), 5);
 
     let file = Some(std::path::PathBuf::from("input-example"));
     let measurements = read_int_lines(file).unwrap();
-    assert_eq!(increase_window_count(&measurements, 3), 5);
+    assert_eq!(count_window_increase(&measurements, window), 5);
 }
 
 #[test]
 fn part1_actual() {
     let file = Some(std::path::PathBuf::from("input-actual"));
     let measurements = read_int_lines(file).unwrap();
-    assert_eq!(increase_window_count(&measurements, 1), 1233);
+    let window = 1;
+    assert_eq!(count_window_increase(&measurements, window), 1233);
 }
 
 #[test]
 fn part2_actual() {
     let file = Some(std::path::PathBuf::from("input-actual"));
     let measurements = read_int_lines(file).unwrap();
-    assert_eq!(increase_window_count(&measurements, 3), 1275);
+    let window = 3;
+    assert_eq!(count_window_increase(&measurements, window), 1275);
 }
