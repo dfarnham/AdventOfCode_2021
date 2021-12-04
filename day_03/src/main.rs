47bc32d6d05bd8c663b972ff1eec10d5ca7ff8c2
Dave Farnham <dave.farnham@icloud.com>
2021-12-03 23:00:33 -0700

    refactor

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -3,15 +3,18 @@ use structopt::StructOpt;
 
 // https://adventofcode.com/2021/day/3
 
-fn get_gamma_epsilon(data: &[u32], num_bits: u8) -> (u32, u32) {
+fn get_gamma_epsilon(data: &[u32]) -> (u32, u32) {
     let mut gamma = 0;
     let mut epsilon = 0;
-    let threshold = data.len() as f32 / 2.0;
 
-    for bits in 0..num_bits as u8 {
-        let mask = 2_u32.pow(bits.into()) as u32;
+    // how many bits does the largest value in the dataset occupy
+    let nbits = ((*data.iter().max().expect("max() failure") as f32).log2()).round() as usize;
+    let masks = (0..nbits).into_iter().map(|i| 1 << i).collect::<Vec<u32>>();
+    for bit in 0..nbits {
+        let mask = masks[bit];
         let count = data.iter().filter(|n| (*n & mask) == mask).count();
-        match count as f32 >= threshold {
+        // are there more bits "on" than "off" in this position?
+        match count >= data.len() - count {
             true => gamma |= mask,
             false => epsilon |= mask,
         }
@@ -19,12 +22,15 @@ fn get_gamma_epsilon(data: &[u32], num_bits: u8) -> (u32, u32) {
     (gamma, epsilon)
 }
 
-fn get_oxy_co2(data: &[u32], num_bits: u8) -> (u32, u32) {
+fn get_oxy_co2(data: &[u32]) -> (u32, u32) {
     let mut oxy = data.to_vec();
     let mut co2 = data.to_vec();
 
-    for bits in (0..num_bits).rev() {
-        let mask = 2_u32.pow(bits.into()) as u32;
+    // how many bits does the largest value in the dataset occupy
+    let nbits = ((*data.iter().max().expect("max() failure") as f32).log2()).round() as usize;
+    let masks = (0..nbits).into_iter().map(|i| 1 << i).collect::<Vec<u32>>();
+    for bit in (0..nbits).rev() {
+        let mask = masks[bit];
 
         if oxy.len() > 1 {
             let set1 = oxy
@@ -32,14 +38,13 @@ fn get_oxy_co2(data: &[u32], num_bits: u8) -> (u32, u32) {
                 .filter(|n| (*n & mask) == mask)
                 .copied()
                 .collect::<Vec<u32>>();
-            let set2 = oxy
-                .iter()
-                .filter(|n| (*n & mask) != mask)
-                .copied()
-                .collect::<Vec<u32>>();
-            oxy = match set1.len() >= set2.len() {
+            oxy = match set1.len() >= oxy.len() - set1.len() {
                 true => set1,
-                false => set2,
+                false => oxy
+                    .iter()
+                    .filter(|n| (*n & mask) != mask)
+                    .copied()
+                    .collect::<Vec<u32>>(),
             };
         }
 
@@ -49,14 +54,13 @@ fn get_oxy_co2(data: &[u32], num_bits: u8) -> (u32, u32) {
                 .filter(|n| (*n & mask) != mask)
                 .copied()
                 .collect::<Vec<u32>>();
-            let set2 = co2
-                .iter()
-                .filter(|n| (*n & mask) == mask)
-                .copied()
-                .collect::<Vec<u32>>();
-            co2 = match set1.len() <= set2.len() {
+            co2 = match set1.len() <= co2.len() - set1.len() {
                 true => set1,
-                false => set2,
+                false => co2
+                    .iter()
+                    .filter(|n| (*n & mask) == mask)
+                    .copied()
+                    .collect::<Vec<u32>>(),
             };
         }
     }
@@ -82,16 +86,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // ==============================================================
 
-    let binary_data = read_data_lines::<String>(args.input)?
+    let data = read_data_lines::<String>(args.input)?
         .iter()
         .map(|s| u32::from_str_radix(s, 2).unwrap())
         .collect::<Vec<u32>>();
-    let num_bits = ((*binary_data.iter().max().ok_or("max() failure")? as f32).log2()).round() as u8;
 
-    let (gamma, epsilon) = get_gamma_epsilon(&binary_data, num_bits);
+    let (gamma, epsilon) = get_gamma_epsilon(&data);
+    //println!("gamma = {}, epsilon = {}", gamma, epsilon);
     println!("Answer Part 1 = {}", gamma * epsilon);
 
-    let (oxy, co2) = get_oxy_co2(&binary_data, num_bits);
+    let (oxy, co2) = get_oxy_co2(&data);
+    //println!("oxy = {}, co2 = {}", oxy, co2);
     println!("Answer Part 2 = {}", oxy * co2);
     Ok(())
 }
@@ -99,13 +104,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 fn part1_example() {
     let file = Some(std::path::PathBuf::from("input-example"));
-    let binary_data = read_data_lines::<String>(file)
+    let data = read_data_lines::<String>(file)
         .unwrap()
         .iter()
         .map(|s| u32::from_str_radix(s, 2).unwrap())
         .collect::<Vec<u32>>();
-    let num_bits = ((*binary_data.iter().max().unwrap() as f32).log2()).round() as u8;
-    let (gamma, epsilon) = get_gamma_epsilon(&binary_data, num_bits);
+    let (gamma, epsilon) = get_gamma_epsilon(&data);
     assert_eq!(gamma, 22);
     assert_eq!(epsilon, 9);
     assert_eq!(gamma * epsilon, 198);
@@ -114,26 +118,24 @@ fn part1_example() {
 #[test]
 fn part1_actual() {
     let file = Some(std::path::PathBuf::from("input-actual"));
-    let binary_data = read_data_lines::<String>(file)
+    let data = read_data_lines::<String>(file)
         .unwrap()
         .iter()
         .map(|s| u32::from_str_radix(s, 2).unwrap())
         .collect::<Vec<u32>>();
-    let num_bits = ((*binary_data.iter().max().unwrap() as f32).log2()).round() as u8;
-    let (gamma, epsilon) = get_gamma_epsilon(&binary_data, num_bits);
+    let (gamma, epsilon) = get_gamma_epsilon(&data);
     assert_eq!(gamma * epsilon, 1307354);
 }
 
 #[test]
 fn part2_example() {
     let file = Some(std::path::PathBuf::from("input-example"));
-    let binary_data = read_data_lines::<String>(file)
+    let data = read_data_lines::<String>(file)
         .unwrap()
         .iter()
         .map(|s| u32::from_str_radix(s, 2).unwrap())
         .collect::<Vec<u32>>();
-    let num_bits = ((*binary_data.iter().max().unwrap() as f32).log2()).round() as u8;
-    let (oxy, co2) = get_oxy_co2(&binary_data, num_bits);
+    let (oxy, co2) = get_oxy_co2(&data);
     assert_eq!(oxy, 23);
     assert_eq!(co2, 10);
     assert_eq!(oxy * co2, 230);
@@ -142,12 +144,11 @@ fn part2_example() {
 #[test]
 fn part2_actual() {
     let file = Some(std::path::PathBuf::from("input-actual"));
-    let binary_data = read_data_lines::<String>(file)
+    let data = read_data_lines::<String>(file)
         .unwrap()
         .iter()
         .map(|s| u32::from_str_radix(s, 2).unwrap())
         .collect::<Vec<u32>>();
-    let num_bits = ((*binary_data.iter().max().unwrap() as f32).log2()).round() as u8;
-    let (oxy, co2) = get_oxy_co2(&binary_data, num_bits);
+    let (oxy, co2) = get_oxy_co2(&data);
     assert_eq!(oxy * co2, 482500);
 }
